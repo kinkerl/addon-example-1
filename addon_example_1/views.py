88e57198389c3629c100e9f8855a2d60d06c142f
Dennis Schwertel <dennisschwertel@gmail.com>
2019-03-15 14:42:08 +0100

    printing env notes

--- addon_example_1/views.py
+++ addon_example_1/views.py
@@ -1,7 +1,12 @@
 
 # pages/views.py
 from django.http import HttpResponse
-
+import os
 
 def index(request):
-    return HttpResponse('Hello, World!')
+    return HttpResponse("""
+    Hello, World!<br/>
+
+    buildpack-notes:{}
+    """.format(os.environ['NOTES'])
+    )
